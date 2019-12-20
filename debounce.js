77671afabd1d020815262316d1946ddbf0471c4f
stackblock <lambdaTL.anthony@gmail.com>
2019-12-20 15:08:31 -0500

    debounce, etc installed

---
+++ debounce.js
@@ -0,0 +1,14 @@
+const deBounce = (func, delay = 1500) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+  };
+
+module.exports = deBounce;
+
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 
-console.log('I was executed')
+const deBounce = require('./debounce.js')
+// const deBounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const chalk = require("chalk");
+const log = console.log;
 
+
+const start = deBounce(() => {
+  log(chalk.blue("Starting users program"));
+}, 100);
+
+chokidar
+  .watch(".")
+  .on("add", start)
+  .on("change", () => log(chalk.yellow("File changed")))
+  .on("unlink", () => log(chalk.red("File unlinked")));
+
+
+
+
+// deBouce function in at deBounce.js AND lodash.debounce is installed for use
+// currently using the deBounce.js function
